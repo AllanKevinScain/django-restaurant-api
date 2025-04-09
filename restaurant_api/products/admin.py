28543b4478c6670ac00495db2c182564716cf400
flamarionfp <flamarion.fagundes@lighthouseit.com.br>
2025-04-09 10:01:44 -0300

    ajustes no nome das classes

--- restaurant_api/products/admin.py
+++ restaurant_api/products/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from .models import Categorias, Produtos
 
-admin.site.register(Categorias)
+
+@admin.register(Categorias)
+class CategoriasAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
 
 
 @admin.register(Produtos)
-class AlternativaAdmin(admin.ModelAdmin):
+class ProdutosAdmin(admin.ModelAdmin):
     list_display = ("categoria", "nome", "preco", "descricao")
     list_filter = ("categoria",)  # a , aponto que é uma tupla
